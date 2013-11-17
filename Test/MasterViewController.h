e9e65ecab2669e42cb2def38944673a344b4d7f9
d <d@ds-Mac.local>
2013-11-17 09:52:56 -0800

    Initial Commit

---
+++ Test/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Test
+//
+//  Created by d on 11/17/13.
+//  Copyright (c) 2013 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
---
+++ Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test
+//
+//  Created by d on 11/17/13.
+//  Copyright (c) 2013 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
